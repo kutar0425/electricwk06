6aede5a1f8ce956a0116409ebf7e366a5a7fa699
kutar0425 <43208473+kutar0425@users.noreply.github.com>
2018-10-31 08:34:58 +0800

    Add files via upload

---
+++ HelloServer.ino
@@ -0,0 +1,92 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+const char* ssid = "IOT_LAB";
+const char* password = "password";
+
+String website;
+WebServer server(80);
+
+const int led = 13;
+
+void buildWeb(){
+  website += "<html>";
+  website += "<head>\n";
+  website += "<meta charset='UTF-8'/>\n";
+  website += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
+  website += "<title>Remote Control LED</title>\n";
+  website += "<style>";
+  website += "html{background-color:#000;}";
+  website += ".title{text-align:center;color:#77BB34;text-shadow:0px 3px 5px #000; margin-bottom: 50px;}";
+  website += "a{display:block;text-decoration:none;background-color:#FFFFFF;radius:3px; ";
+  website += "width:150px;text-align:center;margin:0 auto;margin-bottom:30px;font-size:2em;box-shadow: 0px 3px 6px #000;}";
+  website += ".btn1{color:#659700;}";
+  website += ".btn2{color:#A8A8A8;}";
+  website += "</style>";
+  website += "</head>\n";
+  website += "<body>";
+  website += "<h1 class=\"title\">Remote LED Control</h1>";
+  website += "<a class=\"btn1\" href=\"/H\">ON</a>";
+  website += "<a class=\"btn2\" href=\"/L\">OFF</a>";
+  website += "</body></html>";
+ }
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/html", website);
+  digitalWrite(led, 0);
+}
+void High() {
+  digitalWrite(led, 1);
+}
+void Low() {
+  digitalWrite(led, 0);
+}
+void handleNotFound() {
+  digitalWrite(led, 1);
+  server.send(404, "text/html", "Page not founded...");
+  digitalWrite(led, 0);
+}
+
+void setup(void) {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  buildWeb();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.on("/H", High);
+  server.on("/L", Low);
+  
+  server.on("/inline", []() {
+    server.send(200, "text/html", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+}
